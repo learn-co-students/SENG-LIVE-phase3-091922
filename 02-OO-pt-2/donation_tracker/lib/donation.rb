e68866c09ca12548d0cfaa826ab6a5a394087747
aisayo <aisayo@icloud.com>
2022-11-02 13:48:41 -0400

    solution

--- 02-OO-pt-2/donation_tracker/lib/donation.rb
+++ 02-OO-pt-2/donation_tracker/lib/donation.rb
@@ -1,21 +1,32 @@
+# Agenda:
+# - Discuss mass assignment 
+# - Complete Donation#print
+# - Discuss class variables and methods
+# - Discuss inheritance
+
 require 'pry'
 
 class Donation 
   
-  attr_accessor :org, :amt, :date
+  attr_accessor :org, :amount, :date, :donor
 
-  def initialize(org, amt, date) 
+  def initialize(org, amount, date, donor) 
     @org = org
-    @amt = amt
+    @amount = amount
     @date = date
+    @donor = donor
   end
 
   def print 
     puts "**************"
-    puts self.amt
+    puts self.amount
     puts self.org 
     puts self.date 
     puts "**************"
   end
 
 end
+
+
+feeding_america = Donation.new('Feeding America', 100, "10/12/22", "Aysan")
+ahs = Donation.new(100, "10/12/22", 'American Humane Society', "Cookie")
